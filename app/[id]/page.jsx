fa9ad516118516a7d55999c29e9c5764d54c07cd
Saif <faysalbakziz@gmail.com>
2023-07-30 16:16:17 +0100

    replaced getStaticPaths with generateStaticParams

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -1,23 +1,14 @@
-import Head from "next/head";
 import { getAllPostIds, getPostData } from "../../lib/posts";
-// import { getStaticProps } from "next";
 
-export async function getStaticPaths() {
+export async function generateStaticParams() {
   const paths = getAllPostIds();
-  return {
-    paths,
-    fallback: false,
-  };
+  return paths;
 }
 export default async function Post({ params }) {
   const postData = await getPostData(params.id);
 
-  console.log(postData.title);
   return (
     <>
-      {/* <Head>
-        <title>{params.id}</title>
-      </Head> */}
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </>
   );
